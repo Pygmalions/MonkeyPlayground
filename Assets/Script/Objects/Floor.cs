84f72070a518987ecd2325763c9bab60727b50ec
Haoyu Jia <jia_vincent@outlook.com>
2025-09-13 21:12:19 +0900

    Fix generating FloorData.

--- Assets/Script/Objects/Floor.cs
+++ Assets/Script/Objects/Floor.cs
@@ -1,3 +1,4 @@
+using System;
 using MonkeyPlayground.Data;
 using UnityEngine;
 
@@ -6,9 +7,27 @@ namespace MonkeyPlayground.Objects
     [DisallowMultipleComponent]
     public class Floor : PerceptibleObject<FloorData>
     {
+        private int _leftEndX;
+        private int _rightEndX;
+        private int _y;
+
+        private void Update()
+        {
+            var position = transform.position;
+            var size = transform.localScale;
+            _leftEndX = (int)(position.x - size.x / 2.0f);
+            _rightEndX = (int)(position.x + size.x / 2.0f);
+            _y = (int)MathF.Ceiling(position.y + size.y / 2.0f);
+        }
+        
         protected override FloorData OnGenerateData()
         {
-            return new FloorData(transform);
+            return new FloorData
+            {
+                LeftEndX = _leftEndX,
+                RightEndX = _rightEndX,
+                Y = _y
+            };
         }
     }
 }
