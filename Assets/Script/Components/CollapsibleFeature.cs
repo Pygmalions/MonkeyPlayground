0fe4585bb9b696ecb3fc029d0c255f98b0cd1741
YakarifromCN <yakari0leo@gmail.com>
2025-09-14 19:46:15 +0900

    Add CollapsibleFeature.cs and InvisibleFeature.cs

--- Assets/Script/Components/CollapsibleFeature.cs
+++ Assets/Script/Components/CollapsibleFeature.cs
@@ -1,10 +1,80 @@
+using System;
+using System.Collections;
+using MonkeyPlayground.Data;
 using UnityEngine;
 
 namespace MonkeyPlayground.Components
-{
-	public class CollapsibleFeature
-	{
-    
-	}
-}
+{ 
+    [RequireComponent(typeof(Collider2D))] 
+    public class CollapsibleFeature : MonoBehaviour
+    {
+        [SerializeField] private float squashFactor = 0.5f;
+        [SerializeField] private float squashDuration = 0.1f;
+        [SerializeField] private float restoreDuration = 0.2f;
 
+        [SerializeField] private float restoreDelay = 0.5f;
+
+        private Vector3 _originalScale;
+        private Coroutine _animationCoroutine;
+        private Coroutine _restoreDelayCoroutine;
+
+        private void Start()
+        {
+            _originalScale = transform.localScale;
+        }
+
+        /// <summary>
+        /// Public method: Trigger the squash
+        /// </summary>
+        public void TriggerSquash()
+        {
+            if (_restoreDelayCoroutine != null)
+            {
+                StopCoroutine(_restoreDelayCoroutine);
+                _restoreDelayCoroutine = null;
+            }
+
+            if (_animationCoroutine != null) StopCoroutine(_animationCoroutine);
+            _animationCoroutine = StartCoroutine(ChangeScale(_originalScale * new Vector2(1, squashFactor)));
+        }
+
+        /// <summary>
+        /// Public method: Trigger the resumption after a delay
+        /// </summary>
+        public void TriggerRestore()
+        {
+            if (_restoreDelayCoroutine != null) StopCoroutine(_restoreDelayCoroutine);
+            _restoreDelayCoroutine = StartCoroutine(RestoreAfterDelay());
+        }
+
+        private IEnumerator RestoreAfterDelay()
+        {
+            yield return new WaitForSeconds(restoreDelay);
+            if (_animationCoroutine != null) StopCoroutine(_animationCoroutine);
+            _animationCoroutine = StartCoroutine(ChangeScale(_originalScale));
+            _restoreDelayCoroutine = null;
+        }
+
+        private IEnumerator ChangeScale(Vector3 targetScale)
+        {
+            Vector3 startScale = transform.localScale;
+            float duration = targetScale == _originalScale ? restoreDuration : squashDuration;
+            
+            float startY = transform.position.y;
+            float endY = startY - (startScale.y - targetScale.y) * 0.5f;
+
+            float elapsedTime = 0;
+            while (elapsedTime < duration)
+            {
+                float progress = elapsedTime / duration;
+                transform.localScale = Vector3.Lerp(startScale, targetScale, progress);
+                transform.position = new Vector3(transform.position.x, Mathf.Lerp(startY, endY, progress), transform.position.z);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.localScale = targetScale;
+            transform.position = new Vector3(transform.position.x, endY, transform.position.z);
+            _animationCoroutine = null;
+        }
+    }
+}
---
+++ Assets/Script/Components/InvisibleFeature.cs
@@ -0,0 +1,106 @@
+using MonkeyPlayground.Data;
+using MonkeyPlayground.Objects;
+using UnityEngine;
+
+namespace MonkeyPlayground.Components
+{
+    [RequireComponent(typeof(Item), typeof(SpriteRenderer))]// 确保此脚本总是和Item、SpriteRenderer一起使用 
+    public class InvisibleFeature : MonoBehaviour
+    {
+        [SerializeField] private float revealRadius = 2f;
+
+        private Item _item;
+        private SpriteRenderer _spriteRenderer;
+        private Transform _monkeyTransform;
+
+        private Color _originalColor;
+        private bool _isObscured = true;
+
+        private void Awake()
+        {
+            _item = GetComponent<Item>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            var monkey = Object.FindFirstObjectByType<Monkey>();
+            if (monkey != null)
+            {
+                _monkeyTransform = monkey.transform;
+            }
+            else
+            {
+                Debug.LogError("No monkey found in the scene, ObscurableItem doesn't work!", this);
+                enabled = false;//没有猴子这个脚本没必要工作吧
+                return;
+            }
+
+            _originalColor = _spriteRenderer.color;
+
+            _item.OnGeneratingData += ModifyItemData;
+        }
+
+        private void OnDestroy()
+        {
+            if (_item != null)
+            {
+                _item.OnGeneratingData -= ModifyItemData;
+            }
+        }
+
+        private void Update()
+        {
+            if (_monkeyTransform == null) return;
+
+            float distanceToMonkey = Vector2.Distance(transform.position, _monkeyTransform.position);
+
+            if (distanceToMonkey <= revealRadius)
+            {
+                Reveal();
+            }
+            else
+            {
+                Obscure();
+            }
+        }
+
+        private void Obscure()
+        {
+            _isObscured = true;
+            _spriteRenderer.color = Color.gray;
+        }
+
+        private void Reveal()
+        {
+            _isObscured = false;
+            _spriteRenderer.color = _originalColor;
+        }
+
+        /// <summary>
+        /// Method that responds to the OnGeneratingData event
+        /// </summary>
+        private ItemData ModifyItemData(ItemData originalData)
+        {
+            if (_isObscured)
+            {
+                return new ItemData
+                {
+                    Id = originalData.Id,
+                    Name = "Unknown",
+                    Description = "An unknown object, you can only know what it is when you get closer",
+                    Position = originalData.Position,
+                    Size = originalData.Size,
+                    Functions = originalData.Functions
+                };
+            }
+    
+            return originalData;
+        }
+
+        /// <summary>
+        /// Draw a visual range circle in the editor to facilitate debugging
+        /// </summary>
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position, revealRadius);
+        }
+    }
+}
